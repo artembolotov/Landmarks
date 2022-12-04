964efe02df9d5ecb3208d514d0712594d41139b4
Artem Bolotov <teeemon@gmail.com>
2022-12-04 08:57:34 +0300

    Added show favorites toggle and behaviour

--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
+    @State private var region = MKCoordinateRegion()
     
     var coordinate: CLLocationCoordinate2D
     
